0c01c26d7ebec0261d94991c1c940a6f1e15d5a9
Vaishnavi-Kharat <vaishnavikharat2020@gmail.com>
2024-07-31 19:25:15 +0530

    javascript variable
    javascript variable

---
+++ 01-variable-assignment.js
@@ -0,0 +1,14 @@
+var cityName;
+console.log("My city name is - Pandharpur");
+
+var bestFreind;
+console.log("My best freind name is - Siya ");
+
+var countryName;
+console.log("My country name is - India");
+
+var schoolName;
+console.log("My school name is - New English School Bhalwani");
+
+var collegeName;
+console.log("My college name is - RIT,Islampur")
