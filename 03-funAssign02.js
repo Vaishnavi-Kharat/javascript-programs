fdafd7dc5be5dcb4f3005d14fb4e88be600713e7
Vaishnavi-Kharat <vaishnavikharat2020@gmail.com>
2024-08-02 16:41:36 +0530

    03-funAssign02.js
    03-funAssign02.js

---
+++ 03-funAssign02.js
@@ -0,0 +1,31 @@
+console.log("========= With Argument ==========");
+console.log("==================== Bank 1 ===================");
+
+function bankDetails(bankName,accountNum,location,pinCode) {
+    console.log("Bank name is :",bankName);
+    console.log("Account Number is :",accountNum);
+    console.log("Location of bank is :",location);
+    console.log("Pincode number is :",pinCode);
+}
+bankDetails("CITI Bank", 3456782345, " Pune", 421202);
+
+
+console.log("================== Bank 2 =================");
+function bankDetails(bankName,accountNum,location,pinCode) {
+    console.log("Bank name is :",bankName);
+    console.log("Account Number is :",accountNum);
+    console.log("Location of bank is :",location);
+    console.log("Pincode number is :",pinCode);
+}
+bankDetails("Axis Bank", 7856782345, " Mumbia",441202);
+
+
+console.log("=================== Bank 3 ==================");
+function bankDetails(bankName,accountNum,location,pinCode) {
+    console.log("Bank name is :",bankName);
+    console.log("Account Number is :",accountNum);
+    console.log("Location of bank is :",location);
+    console.log("Pincode number is :",pinCode);
+}
+bankDetails("HDFC Bank", 8956782345, " Pune", 631202, " Open");
+
